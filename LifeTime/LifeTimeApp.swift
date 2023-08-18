636b18282f4cf94cd9118e0986a9539856ba7eaf
Chris Lee <chris0803lee@gmail.com>
2023-08-18 16:03:52 +0900

    Initial Commit

---
+++ LifeTime/LifeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeTimeApp.swift
+//  LifeTime
+//
+//  Created by Mac-aroni on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LifeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

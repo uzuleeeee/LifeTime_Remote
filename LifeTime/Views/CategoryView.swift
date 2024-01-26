95240fff4160facb70d79094865ad9f3ad6d0933
uzuleeeee <ulee36@gatech.edu>
2024-01-26 20:42:12 +0900

    Create basic category view layout.

---
+++ LifeTime/Views/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  LifeTime
+//
+//  Created by Mac-aroni on 1/26/24.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
